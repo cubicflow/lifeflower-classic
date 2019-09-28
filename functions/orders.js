3345e12b12d3fda2730ef0e6b0670582f1b4b064
CubicflowDan <dan@cubicflow.com>
2019-09-28 14:24:16 +0900

    Add basic framework for dealing with webhooks

--- functions/orders.js
+++ functions/orders.js
@@ -1,8 +1,45 @@
 
 exports.handler = async function(event, context, callback) {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({hello: 'I love you'}),
+  const data = await parseJson(event.body)
+
+  if (!data) {
+    return error('Request requires json body');
   }
+
+  if (data.eventName === 'order.completed') {
+    const dataForShipping = transformDataForShipping(data)
+    return success(dataForShipping);
+  };
+
+  return error();
+}
+
+const success = function(body, status){
+  return {
+    statusCode: status || 200,
+    body: JSON.stringify(body),
+  }
+}
+
+const error = function(message, status){
+  return {
+    statusCode: status || 400,
+    body: message || 'Unsupported request',
+  }
+}
+
+const parseJson = async function(jsonString){
+  let data;
+  try {
+    data = await JSON.parse(jsonString);
+  } catch (e) {
+    data = null;
+  }
+  return data;
+}
+
+const transformDataForShipping = function(data){
+  // todo
+  return data;
 }
